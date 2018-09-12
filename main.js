8e95cdd767e71ff387c11c861447b2cd753d6f0c
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-09-12 16:34:24 -0600

    finish pixel art

---
+++ main.js
@@ -0,0 +1,34 @@
+var div = document.querySelector("div");
+var body = document.querySelector("body");
+var height = document.querySelector("#rows_height");
+var width = document.querySelector("#col_width");
+var grid = document.querySelector("#createGrid")
+var color = document.querySelector("#palette")
+
+var table = document.createElement("table");
+table.id = "canvas";
+div.appendChild(table);
+
+var canvas = document.querySelector("#canvas");
+
+palette.addEventListener("click", function(){});
+
+grid.onsubmit = function(e) {
+    e.preventDefault()
+    createGrid();
+}
+
+function createGrid() {
+    for (let r = 0; r < height.value; r++) {
+        let row = canvas.insertRow(r);
+        for (let c = 0; c < width.value; c++) {
+            const pixel = row.insertCell(c);
+            pixel.className = "pixel";
+            pixel.addEventListener("click", fillPixel);
+        }
+    }
+}
+
+function fillPixel() {
+    this.setAttribute("style", `background-color: ${color.value}`);
+}
