d8c67f03ee12d94491dad3161348c8a62f175c48
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-10-27 00:20:38 -0600

    fix colors

--- main.js
+++ main.js
@@ -11,24 +11,24 @@ div.appendChild(table);
 
 var canvas = document.querySelector("#canvas");
 
-palette.addEventListener("click", function(){});
+palette.addEventListener("click", function () { });
 
-grid.onsubmit = function(e) {
-    e.preventDefault();
-    createGrid();
-}
+grid.onsubmit = function (e) {
+	e.preventDefault();
+	createGrid();
+};
 
 function createGrid() {
-    for (let r = 0; r < height.value; r++) {
-        let row = canvas.insertRow(r);
-        for (let c = 0; c < width.value; c++) {
-            const pixel = row.insertCell(c);
-            pixel.className = "pixel";
-            pixel.addEventListener("click", fillPixel);
-        }
-    }
-}
+	for (let r = 0; r < height.value; r++) {
+		let row = canvas.insertRow(r);
+		for (let c = 0; c < width.value; c++) {
+			const pixel = row.insertCell(c);
+			pixel.className = "pixel";
+			pixel.addEventListener("click", fillPixel);
+		}
+	}
+};
 
 function fillPixel() {
-    this.setAttribute("style", `background-color: ${color.value}`);
-}
+	this.setAttribute("style", `background-color: ${color.value}`);
+};
